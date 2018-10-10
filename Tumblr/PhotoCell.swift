04478feaf5025bbdd3ac703c44904c564966ee20
Felipe De La Torre <felipe9141@sbcglobal.net>
2018-10-10 03:06:56 -0700

    Lab 1 completed along with optional story

---
+++ Tumblr/PhotoCell.swift
@@ -0,0 +1,30 @@
+//
+//  PhotoCell.swift
+//  Tumblr
+//
+//  Created by Felipe De La Torre on 10/9/18.
+//  Copyright © 2018 Felipe De La Torre. All rights reserved.
+//
+
+import UIKit
+import AlamofireImage
+
+class PhotoCell: UITableViewCell {
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+    
+    // Configure YourCustomCell using the outlets that you've defined.
+ 
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
